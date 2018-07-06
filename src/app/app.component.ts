153b0a81ad73b941a04d47f0eddb007910274c98
Luana Lima <luluanacarla@gmail.com>
2018-07-05 21:15:43 -0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
+import {User} from "./models/user";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+    users: User[];
+    usersFiltered: User[];
+
+    constructor(private http: Http) {
+
+    }
+
+    ngOnInit(){
+      this.getUsers();
+
+    }
+
+    getUsers() {
+        this.http.get('http://jsonplaceholder.typicode.com/users')
+            .subscribe(
+                res => {
+                  console.log("res",res.json());
+                    this.users = res.json();
+                    this.usersFiltered = res.json();
+                    //res => this.users = res.json() as User[]
+                    console.log("users",this.users);
+
+                    this.usersFiltered = this.usersFiltered.sort(function(a, b){
+                        if(a.name < b.name) return -1;
+                        if(a.name > b.name) return 1;
+                        return 0;
+                    })
+                }
+            );
+    }
+
+    onSearchChange(searchValue : string ) {
+        this.usersFiltered = this.users.filter((item) => {
+            let reverseEmail = item.email.split("").reverse().join("");
+            console.log(reverseEmail);
+            let dotIndex = reverseEmail.toLowerCase().indexOf('.');
+            console.log(dotIndex);
+            let emailEnd = reverseEmail.substring(0,dotIndex+1);
+            emailEnd = emailEnd.split("").reverse().join("");
+            console.log(emailEnd);
+            return emailEnd.toLowerCase().indexOf(searchValue.toLowerCase()) > -1;
+
+        });
+        console.log(searchValue);
+    }
 }
---
+++ src/app/models/address.ts
@@ -0,0 +1,10 @@
+import {Geo} from "./geo";
+
+export class Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo:Geo;
+
+}
